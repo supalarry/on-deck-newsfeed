98cc0875cb437c166c4360de549c99171f9f2ecd
Larry <lauris.skraucis@gmail.com>
2021-12-07 20:56:05 +0100

    Add frame for landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,28 +9,30 @@ export default function Home() {
       <Head>
         <title>Newsfeed worth scrolling</title>
       </Head>
-      <Flex  w='100vw' direction="column" h='100vh' bg="#1b263c" display="flex" align="center" justify="center">
-        <ChakraLink href="https://www.beondeck.com/" isExternal position="absolute" top="0">
-            <Image src='/on-deck-logo-and-name.png' borderBottomRadius="15" width='150px'/>
-        </ChakraLink>
-        <Text
-          bgGradient='linear(to-r, #305cea, #773de2, #e95370)'
-          bgClip='text'
-          fontSize='8xl'
-          fontWeight='extrabold'
-          align="center"
-        >
-          Inspire and be inspired.
-        </Text>
-        <Text align="center" fontSize='2xl' color="#fafafa">
-          Our newsfeed is a door to once in a lifetime opportunities and connections.
-        </Text>
-        <Text align="center" fontSize='2xl' color="#fafafa">It's a newsfeed worth scrolling.</Text>
-        <Link href="/newsfeeds">
-          <Button mt={8} color='#fafafa' bg='#773de2' _hover={{ bg: '#e95370' }} size="lg">
-            Read
-          </Button>
-        </Link>
+      <Flex  w='100vw' direction="column" h='100vh' bg="#182135" display="flex" align="center" justify="center">
+        <Flex  w='95vw' direction="column" h='90vh' bg="#1b263c" display="flex" align="center" justify="center" borderRadius="15">
+          <ChakraLink href="https://www.beondeck.com/" isExternal position="absolute" top="0">
+              <Image src='/on-deck-logo-and-name.png' borderBottomRadius="15" width='150px'/>
+          </ChakraLink>
+          <Text
+            bgGradient='linear(to-r, #305cea, #773de2, #e95370)'
+            bgClip='text'
+            fontSize='8xl'
+            fontWeight='extrabold'
+            align="center"
+          >
+            Inspire and be inspired.
+          </Text>
+          <Text align="center" fontSize='2xl' color="#fafafa">
+            Our newsfeed is a door to once in a lifetime opportunities and connections.
+          </Text>
+          <Text align="center" fontSize='2xl' color="#fafafa">It's a newsfeed worth scrolling.</Text>
+          <Link href="/newsfeeds">
+            <Button mt={8} color='#fafafa' bg='#773de2' _hover={{ bg: '#e95370' }} size="lg">
+              Read
+            </Button>
+          </Link>
+        </Flex>
       </Flex>
     </>
   )
