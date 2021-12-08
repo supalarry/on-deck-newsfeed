5a6b22052b538a7e7b64b595184e2949ec096d98
Larry <lauris.skraucis@gmail.com>
2021-12-08 23:42:00 +0100

    [WIP] Create newsfeed container component

--- features/newsfeed/containers/newsfeed.container.tsx
+++ features/newsfeed/containers/newsfeed.container.tsx
@@ -1,7 +1,9 @@
 import Newsfeed from '../components/newsfeed.component';
 import {useQuery, DocumentNode, gql} from '@apollo/client'
 import {useEffect, useState} from 'react'
-import {FellowshipName, User, Project, Announcement, NewsfeedResponse} from '../../../shared/types'
+import {FellowshipName, Post, NewsfeedResponse} from '../../../shared/types'
+import {DB_QUERY_BATCH_SIZE} from '../../../shared/constants'
+import {isUser, isProject, isAnnouncement} from '../helpers/identifyPostType'
 
 type Props = {
   fellowship: FellowshipName;
@@ -11,31 +13,89 @@ type QueryData = {
   newsfeed: NewsfeedResponse;
 }
 
-type Post = User | Project | Announcement
+type QueryVars = {
+  usersOffset: number;
+  projectsOffset: number;
+  announcementsOffset: number;
+}
+
+const queryOffsets = {
+  usersOffset: 0,
+  projectsOffset: 0,
+  announcementsOffset: 0
+}
 
 export default function NewsfeedContainer({fellowship} : Props) {
   const query = getNewsfeedQuery(fellowship);
-  const {data, error, loading} = useQuery<QueryData>(
-    query
+  const {data, error, loading, fetchMore} = useQuery<QueryData, QueryVars>(
+    query,
+    {
+      variables: queryOffsets
+    }
   )
 
   const [posts, setPosts] = useState<Post[] | undefined>();
-
+  const [newPosts, setNewPosts] =useState<Post[] | undefined>();
   
+  function fetchMorePosts() {
+    fetchMore({
+      variables: queryOffsets,
+      updateQuery: (pv, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          return pv;
+        }
+        let postsX = getNewsfeedPosts(fetchMoreResult!.newsfeed);
+        postsX = sortByNewest(postsX);
+        postsX = postsX.slice(0, DB_QUERY_BATCH_SIZE);
+        updateOffsets(postsX, queryOffsets);
+        const combined = [...postsX];
+        setNewPosts(combined);
+        return fetchMoreResult;
+      }
+    })
+  }
+
   useEffect(() => {
-    if (data?.newsfeed && !loading && !error) {
-      const posts = getNewsfeedPosts(data.newsfeed);
+    if (data?.newsfeed && !loading && !error && !posts) {
+      let posts = getNewsfeedPosts(data.newsfeed);
+      posts = sortByNewest(posts);
+      posts = posts.slice(0, DB_QUERY_BATCH_SIZE);
+      updateOffsets(posts, queryOffsets);
       setPosts(posts);
+    } else if (posts) {
+      setPosts([...posts, ...newPosts!]);
     }
-  }, [data, loading, error])
+  }, [data, loading, error, newPosts])
 
   if (!posts || loading || error) {
     return null
   }
 
   return (
-    <Newsfeed posts={posts}/>
+    <Newsfeed posts={posts} handleScrolledToBottom={fetchMorePosts}/>
   )
+}
+
+function updateOffsets(posts: Post[], offsets: QueryVars) {
+  posts.forEach(post => {
+    if (isUser(post)) {
+      offsets.usersOffset += 1;
+    } else if (isProject(post)) {
+      offsets.projectsOffset += 1;
+    } else if (isAnnouncement(post)) {
+      offsets.announcementsOffset += 1;
+    }
+  })
+}
+
+function sortByNewest(posts: Post[]): Post[] {
+  const postsCopy = [...posts];
+  postsCopy.sort((a, b) => {
+    const aSinceEpoch = new Date(a.created_ts).valueOf();
+    const bSinceEpoch = new Date(b.created_ts).valueOf();
+    return bSinceEpoch - aSinceEpoch
+  });
+  return postsCopy;
 }
 
 function getNewsfeedPosts(newsfeed: NewsfeedResponse): Post[] {
@@ -55,9 +115,9 @@ function getNewsfeedPosts(newsfeed: NewsfeedResponse): Post[] {
 function getNewsfeedQuery(fellowship: FellowshipName): DocumentNode {
   const newsfeedQueries = {
     founders: gql`
-      query Query {
+      query newsfeed($usersOffset: Int!, $projectsOffset: Int!, $announcementsOffset: Int!) {
         newsfeed {
-          founders {
+          founders: users(fellowship: "founders", offset: $usersOffset) {
             id
             name
             bio
@@ -69,15 +129,17 @@ function getNewsfeedQuery(fellowship: FellowshipName): DocumentNode {
               description
               icon_url
             }
+            created_ts
           }
-          angels {
+          angels: users(fellowship: "angels", offset: $usersOffset) {
             id
             name
             bio
             fellowship
             avatar_url
+            created_ts
           }
-          projects {
+          projects(offset: $projectsOffset) {
             id
             name
             description
@@ -89,20 +151,22 @@ function getNewsfeedQuery(fellowship: FellowshipName): DocumentNode {
               fellowship
               avatar_url
             }
+            created_ts
           }
-          announcements(fellowship: "founders") {
+          announcements(fellowship: "founders", offset: $announcementsOffset) {
             id
             fellowship
             title
             body
+            created_ts
           }
         }
       }
       `,
     angels: gql`
-      query Query {
+      query newsfeed($usersOffset: Int!, $projectsOffset: Int!, $announcementsOffset: Int!) {
         newsfeed {
-          founders {
+          founders: users(fellowship: "founders", offset: $usersOffset) {
             id
             name
             bio
@@ -114,15 +178,17 @@ function getNewsfeedQuery(fellowship: FellowshipName): DocumentNode {
               description
               icon_url
             }
+            created_ts
           }
-          angels {
+          angels: users(fellowship: "angels", offset: $usersOffset) {
             id
             name
             bio
             fellowship
             avatar_url
+            created_ts
           }
-          projects {
+          projects(offset: $projectsOffset) {
             id
             name
             description
@@ -134,31 +200,35 @@ function getNewsfeedQuery(fellowship: FellowshipName): DocumentNode {
               fellowship
               avatar_url
             }
+            created_ts
           }
-          announcements(fellowship: "angels") {
+          announcements(fellowship: "angels", offset: $announcementsOffset) {
             id
             fellowship
             title
             body
+            created_ts
           }
         }
       }
     `,
     writers: gql`
-      query Query {
+      query newsfeed($usersOffset: Int!, $announcementsOffset: Int!) {
         newsfeed {
-          writers {
+          users(fellowship: "writers", offset: $usersOffset) {
             id
             name
             bio
             fellowship
             avatar_url
+            created_ts
           }
-          announcements(fellowship: "writers") {
+          announcements(fellowship: "writers", offset: $announcementsOffset) {
             id
             fellowship
             title
             body
+            created_ts
           }
         }
       }
