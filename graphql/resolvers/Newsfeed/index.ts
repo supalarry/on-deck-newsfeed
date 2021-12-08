6f72bb764eada3858649e0d93e052ce4208e2a36
Larry <lauris.skraucis@gmail.com>
2021-12-08 23:35:33 +0100

    Remove outdated resolver imports

--- graphql/resolvers/Newsfeed/index.ts
+++ graphql/resolvers/Newsfeed/index.ts
@@ -1,11 +1,7 @@
-import _founders from './founders';
-import _angels from './angels';
-import _writers from './writers';
 import _announcements from './announcements';
 import _projects from './projects';
+import _users from './users';
 
-export const founders = _founders
-export const angels = _angels
-export const writers = _writers
 export const announcements = _announcements
 export const projects = _projects
+export const users = _users
