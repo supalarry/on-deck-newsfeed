1c5af81ec9197ba68ffd55e2c4c287dc031a53f5
Larry <lauris.skraucis@gmail.com>
2021-12-08 11:15:03 +0100

    Use newsletter resolvers

--- graphql/resolvers/Query/index.ts
+++ graphql/resolvers/Query/index.ts
@@ -1,7 +1,9 @@
 import _project from './project'
 import _user from './user'
 import _fellowships from './fellowships'
+import _newsfeed from './newsfeed'
 
 export const project = _project
 export const user = _user
 export const fellowships = _fellowships
+export const newsfeed = _newsfeed
