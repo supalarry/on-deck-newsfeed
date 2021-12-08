75d255d6dde6dbb4908bb1a18b8b1a393adfb329
Larry <lauris.skraucis@gmail.com>
2021-12-08 14:20:46 +0100

    Import projects resolver into newsfeed resolvers

--- graphql/resolvers/Newsfeed/index.ts
+++ graphql/resolvers/Newsfeed/index.ts
@@ -2,8 +2,10 @@ import _founders from './founders';
 import _angels from './angels';
 import _writers from './writers';
 import _announcements from './announcements';
+import _projects from './projects';
 
 export const founders = _founders
 export const angels = _angels
 export const writers = _writers
 export const announcements = _announcements
+export const projects = _projects
