a33bf1015c6dc10e22f5847b5c5d84998efd2ab3
Larry <lauris.skraucis@gmail.com>
2021-12-08 14:58:03 +0100

    Identify button by its name

--- components/ButtonsList.tsx
+++ components/ButtonsList.tsx
@@ -1,20 +1,19 @@
 import { Button, Stack } from '@chakra-ui/react'
 
 interface Button {
-  id: number;
   name: string;
 }
 
-export default function ButtonsList({buttons, handleButtonClick, activeButtonId} : {buttons: Button[], handleButtonClick: (id: number) => void, activeButtonId: number}) {
+export default function ButtonsList({buttons, handleButtonClick, activeButtonName} : {buttons: Button[], handleButtonClick: (name: any) => void, activeButtonName: string}) {
   return (
     <Stack direction='row' spacing={4} align='center'>
         {
           buttons && buttons.map(button => {
             return (
               <Button
-                isActive={button.id === activeButtonId}
-                onClick={() => handleButtonClick(button.id)}
-                key={button.id}
+                isActive={button.name === activeButtonName}
+                onClick={() => handleButtonClick(button.name)}
+                key={button.name}
                 colorScheme='blue'
                 variant='outline'>
                   {button.name}
