16195beecc371b9f5d549bab918d03f1854fe405
Larry <lauris.skraucis@gmail.com>
2021-12-09 18:19:03 +0100

    Clean up landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,46 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { Flex, Link as ChakraLink, Image, Text, Button } from '@chakra-ui/react'
-import { IoMdExit } from '@react-icons/all-files/io/IoMdExit'
+import Head from 'next/head';
+import Link from 'next/link';
+import { Flex, Link as ChakraLink, Image, Text, Button } from '@chakra-ui/react';
+import { IoMdExit } from '@react-icons/all-files/io/IoMdExit';
 
 export default function Home() {
+  const headElementTitle = 'Newsfeed worth scrolling';
+
+  const heading = 'Inspire and be inspired.';
+  const subHeadingOne = 'Our newsfeed is a door to once in a lifetime opportunities and connections.';
+  const subHeadingTwo = 'It\'s a newsfeed worth scrolling.';
+  const newsfeedPageButton = 'Newsfeed';
+
   return (
-    <>
+    <main>
       <Head>
-        <title>Newsfeed worth scrolling</title>
+        <title>{headElementTitle}</title>
       </Head>
-      <Flex  w='100vw' direction="column" h='100vh' bg="#182135" display="flex" align="center" justify="center">
-        <Flex  w='95vw' direction="column" h='90vh' bg="#1b263c" display="flex" align="center" justify="center" borderRadius="15" padding={4}>
-          <ChakraLink href="https://www.beondeck.com/" isExternal position="absolute" top="0" boxShadow='dark-lg' borderBottomRadius="15">
-              <Image src='/on-deck-logo-and-name.png' borderBottomRadius="15" width='120px' opacity='0.9'/>
+      <Flex w='100vw' h='100vh' display="flex" align="center" justify="center"
+        direction="column" bg="#182135">
+        <Flex w='95vw' h='90vh' display="flex" align="center" justify="center"
+          direction="column" padding={4} bg="#1b263c" borderRadius="15">
+          <ChakraLink href="https://www.beondeck.com/" isExternal
+            position="absolute" top="0" boxShadow='dark-lg' borderBottomRadius="15">
+            <Image src='/on-deck-logo-and-name.png'
+            borderBottomRadius="15" width='120px' opacity='0.9'/>
           </ChakraLink>
-          <Text
-            bgGradient='linear(to-r, #305cea, #773de2, #e95370)'
-            bgClip='text'
-            fontSize={{ base: '5xl', md: '7xl', lg: '8xl' }}
-            fontWeight='extrabold'
-            align="center"
-          >
-            Inspire and be inspired.
+          <Text bgGradient='linear(to-r, #305cea, #773de2, #e95370)' bgClip='text'
+            fontSize={{ base: '5xl', md: '7xl', lg: '8xl' }} fontWeight='extrabold' align="center">
+            {heading}
           </Text>
           <Text align="center" mt={{ base: 4, md: 0 }} fontSize={{ base: 'xl', md: '2xl'}} color="#fafafa">
-            Our newsfeed is a door to once in a lifetime opportunities and connections.<br></br>
-            It's a newsfeed worth scrolling.
+            {subHeadingOne} <br/>
+            {subHeadingTwo}
           </Text>
           <Link href="/newsfeeds">
-            <Button mt={8} color='#fafafa' bg='#773de2' _hover={{ bg: '#824ee4' }} size="lg" rightIcon={<IoMdExit />}>
-              Newsfeed
+            <Button color='#fafafa' bg='#773de2' _hover={{ bg: '#824ee4' }}
+              mt={8}  size="lg" rightIcon={<IoMdExit />}>
+              {newsfeedPageButton}
             </Button>
           </Link>
         </Flex>
       </Flex>
-    </>
+    </main>
   )
 }
