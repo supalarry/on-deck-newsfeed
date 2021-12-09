c3979b6927dfa886ff5db8fdfe178fca41f72722
Larry <lauris.skraucis@gmail.com>
2021-12-09 17:58:06 +0100

    Clean up fellowships resolver

--- graphql/resolvers/Query/fellowships.ts
+++ graphql/resolvers/Query/fellowships.ts
@@ -1,11 +1,8 @@
 import db, {FellowshipRow} from '../../db'
 
 export default async function fellowships(parent: unknown): Promise<FellowshipRow[]> {
-  const fellowships: FellowshipRow[] | undefined = await db.getAll(
+  const fellowships: FellowshipRow[] = await db.getAll(
     'SELECT * FROM fellowships'
   )
-  if (!fellowships) {
-    throw new Error('Fellowships not found')
-  }
   return fellowships
 }
