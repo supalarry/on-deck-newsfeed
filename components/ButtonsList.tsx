685ae7e897d65173dfc89df25ff223d5842dc9a0
Larry <lauris.skraucis@gmail.com>
2021-12-10 22:24:16 +0100

    Change menu button styling

--- components/ButtonsList.tsx
+++ components/ButtonsList.tsx
@@ -8,14 +8,27 @@ export default function ButtonsList({buttons, handleButtonClick, activeButtonNam
   return (
     <Stack direction='row' spacing={4} align='center'>
         {
-          buttons && buttons.map(button => {
+          buttons && buttons.map((button, index) => {
             return (
               <Button
                 isActive={button.name === activeButtonName}
                 onClick={() => handleButtonClick(button.name)}
-                key={button.name}
-                colorScheme='blue'
-                variant='outline'>
+                key={`${index}_${button.name}`}
+                textTransform="capitalize"
+                _active={{
+                  fontWeight: 'bold',
+                  opacity: '0.9',
+                }}
+                _hover={{
+                  fontWeight: 'bold',
+                  opacity: '1',
+                  color:'#fafafa',
+                  bgGradient:'linear(to-r, #305cea, #773de2)'
+                }}
+                color='#fafafa'
+                bgGradient='linear(to-r, #305cea, #773de2)'
+                opacity="0.5"
+                variant='solid'>
                   {button.name}
               </Button>
             )
